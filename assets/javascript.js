4209669799b83a3c0016addf6a04d977a3407df1
funduval <fundaduval@gmail.com>
2017-05-18 12:25:07 -0400

    js page

---
+++ assets/javascript.js
@@ -0,0 +1,304 @@
+
+    var talentList = [
+
+
+        {
+            "student":   "The 3rd Grade Glee Club ",
+            "email":   "mariella.trogani@montville.net",
+            "teacher": "Maria Trogani and Various Teachers",
+            "props": "none",
+            "actType": "Choral",
+            "songType": "self-provided track",
+            "artist": "Allessia Cara",
+            "song": "Scars to Your Beautiful",
+            "contact": "Maria Trogani"
+        },
+
+        {
+            "song":   "3 Little Birds",
+            "artist":   "Bob Marley",
+            "student":   "Alaina Berkowitz",
+            "actType":   "Singing",
+            "songType": "karaoke",
+            "teacher":   "Mr. Krygier",
+            "props": "Mic with stand",
+            "contact": "Kim Berkowitz",
+            "email":   "kimberlyberkowitz0805@gmail.com"
+        },
+
+
+
+        {
+            "student":   "Samantha Davis & Hayley Edelstein",
+            "teacher":   "Mr. Krygier",
+            "actType":   "Singing & Piano",
+            "song":   "Lost Boys",
+            "songType": "live instrumental",
+            "props":   "Mic and Piano",
+            "email":   "jessedelstein@gmail.com and rachelamydavis@gmail.com",
+            "contact": "Rachel Davis, Jessica Edelstein"
+        },
+
+        {
+            "student":   "Irene Eldho",
+            "teacher":   "Mr. Brennan",
+            "actType":   "Hula Hoop",
+            "song":   "Hula Hoop",
+            "artist": "(OMI)",
+            "songType": "whole song",
+            "props": "hula hoop",
+            "email":   "anusphilip@gmail.com"
+        },
+
+        {
+            "student":   "Reya Sinha, Heba Imran, Anika Bijala & Irene Eldho",
+            "teacher":   "Mr. Brennan, Mrs. Stephenson",
+            "actType":   "Singing and Dancing",
+            "song":   "Backstage",
+            "artist":   "Denzel",
+            "songType":   "whole song-not karaoke",
+            "props":   "Two microphones",
+            "email":   " neelusinha76@yahoo.com and aparna_chikoti@yahoo.com"
+        },
+
+        {
+            "student":   "Jordan Bergman & Paige Gille",
+            "teacher":   "Abruzzo",
+            "actType":   "Piano and dance",
+            "props": "piano and microphone",
+            "song":   "Do-Re-Mi",
+            "songType": "sung/played live",
+            "artist": "The Sound of Music",
+            "email":   "gille5@optonline.net, jillesq7@yahoo.com"
+        },
+
+        {
+            "student":   "Mateo Takahashi",
+            "teacher":   "Mr. Williams",
+            "song":   "Star Wars theme",
+            "props": "piano",
+            "song":   "Russian sailors dance",
+            "songType": "sung/played live",
+            "actType":   "Playing Piano",
+            "email":   "pastogirl@gmail.com"
+        },
+
+        {
+            "student":   "Sofia Takahashi",
+            "song":   "Firefly",
+            "song":   "Treble A",
+            "props": "piano and mic",
+            "songType": "sung/played live",
+            "actType":   "Singing with Piano",
+            "contact":   "Judith Takahashi",
+            "email":   " pastogirl@gmail.com"
+        },
+
+        {
+            "student":   "Dylan hunter",
+            "teacher": "Krygier",
+            "actType":   "Playing Violin",
+            "song":   " Star-Spangled Banner",
+            "songType": "sung/played live",
+            "props":   " music stand",
+            "contact":   "Darlene hunter",
+            "email":   " thriza@hotmail.com"
+        },
+
+        {
+            "student":   "Tegan Heim & Abigail Lavenberg",
+            "teacher":   "Mrs. Abruzzo",
+            "actType":   "Dance and Gymnastics",
+            "song":   "That's What I Like (clean version)",
+            "artist":   " Bruno Mars",
+            "songType": "whole song-stop at 1:05",
+            "props":   " Rolling chair",
+            "email":   " meganfetters@yahoo.com, geralincastellano@gmail.com"
+        }, {
+            "student":   "Chase Eagan",
+            "teacher":   "Mr. Krygier",
+            "actType":   "Playing Piano",
+            "song":   " the spider dance",
+            "songType": "sung/played live",
+            "props": "piano",
+            "contact":   " Sue Eagan",
+            "email":   " suekeagan@yahoo.com"
+        },
+
+        {
+            "student":   "Jennavecia Simeone",
+            "teacher": "",
+            "actType":   "singing",
+            "song":   " cheap thrills",
+            "props": "Mic and stand",
+            "artist": "Sia",
+            "songType": "karaoke",
+            "contact":   "Jennifer Montanya",
+            "email":   "Jvenice5@yahoo.com"
+        },
+
+        {
+            "student":   "Vivian Wei",
+            "teacher":   "Mrs. Auten",
+            "song":   "A Piano Song",
+            "actType": "Playing Piano",
+            "props": "piano",
+            "songType": "sung/played live",
+            "contact":   "Yu Wang ",
+            "email":   "Angelayuwang@hotmail.com"
+        },
+
+        {
+            "student":   "Corey Wang",
+            "teacher":   " Mr. Brennan",
+            "actType":   "Playing Piano",
+            "props": "piano",
+            "song":   "sonatina in G major",
+            "songType": "sung/played live",
+            "contact":   "David Wayne",
+            "email":   " Laura0@yahoo.com"
+        },
+
+        {
+            "student":   "Alyssa Longobardi",
+            "teacher":   "Mrs. McLaughlin",
+            "actType":   "Singing",
+            "song":   "Part of your World",
+            "songType": "karaoke",
+            "artist":   "The Little Mermaid",
+            "props":   "microphone and music stand",
+            "email":   "jimlongobardi@aol.com"
+        },
+
+        {
+            "student":   "Natalia Fragomeni",
+            "teacher":   "Mr. Wheeler",
+            "actType":   "Singing",
+            "artist":   "Britney Spears",
+            "song":   "Oops I did it again",
+            "songType": "karaoke",
+            "contact":   "Allison",
+            "props": "mic and stand",
+            "email":   " alcreeden@yahoo.com"
+        },
+
+        {
+            "student":   "Priya Ganguli",
+            "teacher":   "Mrs. Abruzzo",
+            "song":   "Memories",
+            "props": "piano",
+            "songType": "sung/played live",
+            "artist": "Cats",
+            "actType":   "Piano",
+            "email": "soho26@aol.com"
+        },
+
+        {
+            "student":   "Sophia Istvan",
+            "teacher": "Mrs. McLaughlin/ Mrs. Amadeo",
+            "actType": "Singing",
+            "song":   "Fight Song",
+            "songType": "karaoke",
+            "artist": "Rachel Platten",
+            "props":   "microphone stand",
+            "email": "funduval@gmail.com"
+        },
+
+        {
+            "student":   "Nora Istvan",
+            "teacher": "Mr. Krygier",
+            "actType": "Singing",
+            "song":   " Space Unicorn",
+            "artist": "Parry Grip",
+            "songType": "karaoke",
+            "props":   " microphone stand",
+            "email": "funduval@gmail.com"
+        },
+
+        {
+            "student":   "Daniel Heras, Jorge Heras",
+            "teacher":   " Mr. Brennan & Mrs. Auten",
+            "actType":   "Singing",
+            "song":   "Below the Surface",
+            "songType": "whole track w/vocals",
+            "artist":   "FNAF sister location",
+            "props":   " custom wig, masks, box",
+            "contact":   " Evelyn Harris",
+            "email":   " evelyn.jdtruckrepair@gmail.com"
+        },
+
+        {
+            "student":   "Delismarie Heras",
+            "teacher":   "Mr. Wheeler",
+            "actType":   "Dance and Lipsynching",
+            "song":   "It's a Hard Knock Life",
+            "songType": "whole track with vocals",
+            "artist":   "Annie",
+            "contact":   "Evelyn Heras",
+            "email":   "evelyn.jdtruckrepair@gmail.com",
+            "props": "microphone and stand"
+
+        },
+
+        {
+            "student":   "Connor Howard",
+            "actType":   "Playing Piano",
+            "songType": "sung/played live",
+            "song":   " Minuet In G",
+            "email":   " joanna@jhoward.us",
+            "props": "piano",
+            "teacher":   "Mrs. Wheeler",
+        },
+
+        {
+            "student":   "Tyler Howard",
+
+            "actType":   "Playing Piano",
+            "songType": "sung/played live",
+            "song":   "William Tel Overture",
+            "props": "piano",
+            "teacher":   "Mrs. Wheeler",
+            "email":   " joanna@jhoward.us"
+
+        },
+
+        {
+            "student":   "Lilah Baker",
+            "actType":   "Dance",
+            "song":   "Issues",
+            "songType": "whole track with vocals",
+            "artist":   "Julia Michaels",
+            "email":   " danbaker12002@yahoo.com",
+            "props": "none",
+            "teacher":   "Mr. Krygier"
+        },
+
+    ]
+
+    for (var i = 0; i < talentList.length; i++) {
+        talentList[i]
+
+
+
+        var text = $("<p>");
+
+        text.addClass("talentItems")
+
+        $("#student-list").append(text);
+
+
+
+        text.append(talentList[i].student.toUpperCase() + ";   " + talentList[i].teacher + ";   " + talentList[i].actType + ";   " + talentList[i].song + " - " + talentList[i].songType + ";   " + "PROPS: " + talentList[i].props);
+
+
+
+        var mailField = $("<p>");
+
+        mailField.addClass("mail")
+
+        $("#email-list").append(mailField);
+
+        mailField.append(talentList[i].email + ",");
+
+    }
+    
